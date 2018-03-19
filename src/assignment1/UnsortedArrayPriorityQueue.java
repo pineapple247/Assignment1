722b816e1023b045f76470351620d49f62db1d99
Kyle <Kyle@Kyle-PC.Home>
2018-03-19 15:55:47 +0000

    First implementation of UnsortedArrayPriorityQueue

---
+++ src/assignment1/UnsortedArrayPriorityQueue.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author Kyle
+ */
+public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
+    
+    /**
+     * Where the data is actually stored.
+     */
+    private final Object[] storage;
+
+    /**
+     * The size of the storage array.
+     */
+    private final int capacity;
+
+    /**
+     * The index of the last item stored.
+     *
+     * This is equal to the item count minus one.
+     */
+    private int tailIndex;
+
+    /**
+     * Create a new empty queue of the given size.
+     *
+     * @param size
+     */
+    
+    public UnsortedArrayPriorityQueue(int size) 
+    {
+
+    }
+    
+    @Override
+    public void add(T item, int priority)
+    {
+        
+    }
+
+    @Override
+    public T head()
+    {
+        
+    }
+    
+    
+    @Override
+    public void remove() 
+    {
+        
+    }
+    
+    @Override
+    public boolean isEmpty()
+    {
+       
+    }
+    
+    @Override
+    public String toString()
+    {
+        
+    }
+}
