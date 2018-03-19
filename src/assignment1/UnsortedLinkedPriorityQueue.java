0dacd7aff753ab8d4fa21c202aacde5702a2269f
Kyle <Kyle@Kyle-PC.Home>
2018-03-19 16:50:52 +0000

    First implementation of UnsortedLinkedPriorityQueue

---
+++ src/assignment1/UnsortedLinkedPriorityQueue.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author 13005626
+ */
+public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T>
+{
+    
+    public UnsortedLinkedPriorityQueue(int size) 
+    {
+
+    }
+    
+    @Override
+    public void add(T item, int priority)
+    {
+        
+    }
+
+    @Override
+    public T head()
+    {
+        
+    }
+    
+    @Override
+    public void remove() 
+    {
+        
+    }
+    
+    @Override
+    public boolean isEmpty()
+    {
+       
+    }
+    
+    @Override
+    public String toString()
+    {
+        
+    }
+}
